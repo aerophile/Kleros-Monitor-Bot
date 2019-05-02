963c78d0c1db61ec21043052731fd1a80ddef8e6
aerophile <shubham0075@gmail.com>
2019-05-03 02:06:35 +0530

    added get_dispute_max function

--- jurors.py
+++ jurors.py
@@ -5,13 +5,30 @@ import os
 from kleros import Kleros, KlerosDispute, KlerosVote
 from collections import Counter, defaultdict
 import pprint
+import requests
 
 node_url = os.environ["ETH_NODE_URL"]
 
 pp = pprint.PrettyPrinter(indent=2)
 case_Number = 1
 juror_accounts = []
-while case_Number < 63:
+
+def get_dispute_max():
+    url = "https://api.etherscan.io/api?" \
+    "module=logs&action=getLogs" \
+    "&fromBlock=379224&toBlock=latest" \
+    "&address=0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069" \
+    "&topic0=0x141dfc18aa6a56fc816f44f0e9e2f1ebc92b15ab167770e17db5b084c10ed995" \
+    "&apikey=YourApiKeyToken"
+    response = requests.get(url = url)
+    dict_json_response = response.json()
+    dispute_num_hex = dict_json_response["result"][-1]['topics'][1]
+    dispute_num_decimal = int(dispute_num_hex, 16)
+    return dispute_num_decimal
+
+total_dispute = get_dispute_max()
+
+while case_Number < total_dispute:
     dispute = KlerosDispute(case_Number, node_url=node_url)
     appeal = len(dispute.rounds) - 1
     jurors = dispute.rounds[-1]
